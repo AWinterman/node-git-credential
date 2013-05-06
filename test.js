09ed890c85b9d812f88e0b0d5c40a1ca4c8bd3af
AWinterman <andywinterman@gmail.com>
2013-05-05 21:41:54 -0700

    finsihed test

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 // just the simplest smoke test
-var get_credential = require("git-credential")
+var get_credential = require("./index")
 
 get_credential(function(err, data){
   if (err) console.log("uh oh! \n" + err)
+  console.log(data)
 })
